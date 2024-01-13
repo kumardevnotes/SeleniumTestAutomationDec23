eae8a0eede429c3e19f22897a6ae900525db4093
kumardevnotes <kumar.thoudishetti@gmail.com>
2024-01-13 23:17:32 +0530

    added all the files gfor the firstime into github

---
+++ src/test/java/JavaDemo.java
@@ -0,0 +1,18 @@
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		// ctrl + spacebar
+        //System.out.println("Hello All!");
+		
+		int personAge =  16; // Integer
+		
+		if(personAge >= 18) // 16 >= 18 --> false
+			System.out.println("Person is a major");
+		else
+			System.out.println("Person is a minor");
+		
+		//below line is commented using 2 forward slashes
+		System.out.println("Bye All!");
+	}
+}
